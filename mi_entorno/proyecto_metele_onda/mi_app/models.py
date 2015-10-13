77c479132ee61fd965d6de53b9b75d1ed2da8310
mario maidana <mariomaidana82@gimail.com>
2015-10-12 22:18:21 -0300

    Modelo- se crearon las clases Establecimiento, ciudad, calificacion
    Se deben revisar las relaciones entre las clases Establecimiento-Calificacion y Clalificacion-Usuario

--- mi_entorno/proyecto_metele_onda/mi_app/models.py
+++ mi_entorno/proyecto_metele_onda/mi_app/models.py
@@ -8,7 +8,6 @@ class Rubro(models.Model):
 	#----------// ATRIBUTOS  //--------#
 	nombre = models.CharField(max_length = 200)
 
-
 	def __unicode__(self):
 		return self.name
 ##----// **fin class**  //-----------------------##
@@ -21,10 +20,9 @@ class Usuario(models.Model):
 	tw_id     = models.CharField(max_length = 200)
 	google_id = models.CharField(max_length = 200)
 
-
 	def __unicode__(self):
 		return self.name
-##----// **Clase USUARIO**  //-----------------------##
+##----// **fin class**  //-----------------------##
 
 
 ##----// **Clase PROVINCIA**  //-----------------------##
@@ -32,10 +30,62 @@ class Provincia(models.Model):
 	#----------// ATRIBUTOS  //--------#
 	nombre = models.CharField(max_length = 200)
 
+	def __unicode__(self):
+		return self.name
+##----// **fin class**  //-----------------------##
+
+
+##----// **Clase PROVINCIA**  //-----------------------##
+class Ciudad(models.Model):
+	#----------// ATRIBUTOS  //--------#
+	nombre        = models.CharField(max_length = 200)
+	codigo_postal = models.CharField(max_length = 10)
+	#--relacion muchos a uno con la clase Provincia--#
+	#FK
+	provincia = models.Foreignkey(Provincia)
 
 	def __unicode__(self):
 		return self.name
-##----// **Clase USUARIO**  //-----------------------##
+##----// **fin class**  //-----------------------##
+
+
+##----// **Clase ESTABLECIOMIENTO**  //-----------------------##
+class Establecimiento(models.Model):
+	#----------// ATRIBUTOS  //--------#
+	nombre    = models.CharField(max_length = 200)
+	direccion = models.CharField(max_length = 200)
+	latitud   = models.FloatField()
+	longitud  = models.FloatField()
+
+	#--relacion muchos a uno con la clase Establecimiento--#
+	#FK
+	ciudad = models.Foreignkey(Ciudad)
+	#--relacion muchos a uno con la clase Rubro--#
+	#FK
+	rubro = models.Foreignkey(Rubro)
+	#--relacion muchos a uno con la clase Calificacion--#
+	#FK 
+	calificaciones = models.Foreignkey(Calificacion)           #<<<<CONSULTAR relacion
+	#Consulta 
+	
+	def __unicode__(self):
+		return self.name
+##----// **fin class**  //-----------------------##
+
+##----// **Clase CALIFICACION**  //-----------------------##
+class Calificacion(models.Model):
+	#----------// ATRIBUTOS  //--------#
+	puntaje    = models-IntegerField()
+	comentario = models.TextField()
+
+	# Consultar estas relaciones relacion!!!!! 
+	usuario    = models.OneToOneField(Usuario)               #<<<<CONSULTAR relacion
+	establecimiento= models.OneToOneField(Establecimiento)   #<<<<CONSULTAR relacion
+	#--------------// //---------------------#
+	def __unicode__(self):
+		return self.comentario
+##----// **fin class**  //-----------------------##
+
 
 	
 
